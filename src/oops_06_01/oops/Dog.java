a067794476c7bb37206b37dfb57740dd2da04c14
Niet <lalitjisharma37@gmail.com>
2024-09-06 17:46:29 +0530

    ATB5xJavaPrograms

---
+++ src/oops_06_01/oops/Dog.java
@@ -0,0 +1,26 @@
+package src.oops_06_01.oops;
+
+public class Dog {
+    //Constructor
+    Dog(){
+        
+    }
+
+
+    //Attributes/Data memeber/variable
+
+    String name;
+
+    //Methods/ Behaviour
+    void eat(){
+        System.out.println("End");
+    }
+//    static int age = 10;
+//    //Function
+//    static void printdetail(){
+//        System.out.println("I am Static");
+//    }
+//    class SmallDog{
+//
+//    }
+}
